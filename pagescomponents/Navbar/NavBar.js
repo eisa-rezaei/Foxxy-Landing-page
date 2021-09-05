6deca0a6a63ca7ba88c8f40e3049c50bf307da14
jesus re <eisa.eisa.r.r@gmail.com>
2021-09-05 23:28:00 +0430

    nav link

--- pagescomponents/Navbar/NavBar.js
+++ pagescomponents/Navbar/NavBar.js
@@ -1,5 +1,5 @@
-import { useRouter } from "next/dist/client/router";
-import Link from "next/link";
+import { useRouter } from "next/router";
+
 import {
   StyledPageNavBar,
   StyledPageNavBarLinks,
