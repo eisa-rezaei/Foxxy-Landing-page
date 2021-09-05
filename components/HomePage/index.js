b8d118c3dfb604b05c3eaf9e1d7083e9fca3ccf8
jesus re <eisa.eisa.r.r@gmail.com>
2021-09-05 22:13:06 +0430

    resolve

--- components/HomePage/index.js
+++ components/HomePage/index.js
@@ -1,17 +1,19 @@
-import LinkBarPart from "./components/LinkBar/index";
-import FeaturedPart from "./components/Featured/index";
-import CreativePart from "./components/Creative/index";
-import PlatformPart from "./components/Platform/index";
+// import EarlyAccsessPart from "./components/EarlyAccsess/index";
+// import LinkBarPart from "./components/LinkBar/index";
+// import FeaturedPart from "./components/Featured/index";
+// import CreativePart from "./components/Creative/index";
+// import PlatformPart from "./components/Platform/index";
 
 import { StyledHomePageContainer } from "./styles";
 
 const HomePage = () => {
   return (
     <StyledHomePageContainer>
+      {/* <EarlyAccsessPart />
       <LinkBarPart />
       <FeaturedPart />
       <CreativePart />
-      <PlatformPart />
+      <PlatformPart /> */}
     </StyledHomePageContainer>
   );
 };
